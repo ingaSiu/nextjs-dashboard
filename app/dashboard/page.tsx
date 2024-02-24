092c90000e077504b2214c4f19594591a23998c3
ingaSiu <ingasiud@gmail.com>
2024-02-24 18:18:57 +0200

    fetch latest invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
+import { fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
+
 import { Card } from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import { fetchRevenue } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 
 export default async function Page() {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -23,7 +25,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
